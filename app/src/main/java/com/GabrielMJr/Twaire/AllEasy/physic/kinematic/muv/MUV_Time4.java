ee67535076ac703478f6d1e8da8494928eccfa34
gabrielmjr <gmjplayys@gmail.com>
2022-10-30 21:20:13 +0200

    1.2.3-Beta_2-initialized kinematic_muv_time, added icon for multi-screen

---
+++ app/src/main/java/com/GabrielMJr/Twaire/AllEasy/physic/kinematic/muv/MUV_Time4.java
@@ -0,0 +1,23 @@
+package com.GabrielMJr.Twaire.AllEasy.physic.kinematic.muv;
+
+import android.os.Bundle;
+import com.GabrielMJr.Twaire.AllEasy.app.MyActivity;
+import com.GabrielMJr.Twaire.AllEasy.R;
+import android.support.v7.widget.Toolbar;
+
+public class MUV_Time4 extends MyActivity {
+
+
+    private void initialize()
+    {
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kinematic_muv_time1);
+        initialize();
+    }
+
+}
