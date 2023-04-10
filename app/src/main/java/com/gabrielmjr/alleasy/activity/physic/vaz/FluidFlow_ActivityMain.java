79ffa99e0ee5a82f1ffd0bda0e7f86da2daeda76
Gabriel M Júnior <gmjplayys@gmail.com>
2023-04-10 06:36:00 +0200

    Re-added fluid flow subtitle list

--- app/src/main/java/com/gabrielmjr/alleasy/activity/physic/vaz/FluidFlow_ActivityMain.java
+++ app/src/main/java/com/gabrielmjr/alleasy/activity/physic/vaz/FluidFlow_ActivityMain.java
@@ -2,18 +2,73 @@ package com.gabrielmjr.alleasy.activity.physic.vaz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.gabrielmjr.alleasy.R;
 import com.gabrielmjr.alleasy.activity.BaseActivity;
+import com.gabrielmjr.alleasy.activity.physic.kinematic.mru.UniformRectilinearMovementActivity;
+import com.gabrielmjr.alleasy.activity.physic.kinematic.muv.VariatedUniformMovementActivity;
+import com.gabrielmjr.alleasy.adapter.SubTitlesAdapter;
+import com.gabrielmjr.alleasy.adapter.SubTitlesAdapter.OnSubtitleClickListener;
+import com.gabrielmjr.alleasy.model.Activity;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FluidFlow_ActivityMain extends BaseActivity {
+public class FluidFlow_ActivityMain extends BaseActivity implements Runnable,
+OnSubtitleClickListener {
+    private Toolbar toolbar;
+    private RecyclerView subTitlesRecycler;
+    private SubTitlesAdapter subTitlesAdapter;
+    private List<Activity> subTitles;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeActivity();
+        getViews();
+        Handler handler = new Handler();
+        handler.postDelayed(this, 150);
+    }
+    
+    private void initializeActivity() {
         setContentView(R.layout.activity_subtitles);
-        
+    }
+
+    private void getViews() {
+        toolbar = findViewById(R.id.toolbar);
+        subTitlesRecycler = findViewById(R.id.subtitles_recycler_view);
+    }
+
+    @Override
+    public void run() {
+        initializeAttributes();
+        buildRecyclerView();
+    }
+
+    private void initializeAttributes() {
+        subTitles = new ArrayList<>();
+        subTitlesAdapter = new SubTitlesAdapter(getApplicationContext(), subTitles, this);
+        setToolBar(toolbar);
+    }
+
+    private void buildRecyclerView() {
+        subTitles.add(new Activity(getText(R.string.fluid_flow_area), Area1.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_flow_rate1), FlowRate1.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_flow_rate2), FlowRate2.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_flow_rate3), FlowRate3.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_ray), Ray1.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_velocity1), Speed1.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_velocity2), Speed2.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_time), Time1.class));
+        subTitles.add(new Activity(getText(R.string.fluid_flow_volume), Volume1.class));
+        subTitlesRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        subTitlesRecycler.setAdapter(subTitlesAdapter);
+    }
+
+    @Override
+    public void onSubTitleClick(int position) {
+        startActivity(new Intent(getApplicationContext(), subTitles.get(position).getClassId()));
     }
 }
