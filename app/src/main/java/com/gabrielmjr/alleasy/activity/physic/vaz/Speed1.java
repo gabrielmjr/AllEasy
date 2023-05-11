075f40245484a39d2335ee5663775b79cda3718b
GabrielMJr <gmjplayys@gmail.com>
2023-05-11 12:50:03 +0200

    Removed obsolete code in flow fluid >> speed 1

--- app/src/main/java/com/gabrielmjr/alleasy/activity/physic/vaz/Speed1.java
+++ app/src/main/java/com/gabrielmjr/alleasy/activity/physic/vaz/Speed1.java
@@ -9,108 +9,13 @@ import com.gabrielmjr.alleasy.R;
 import com.gabrielmjr.alleasy.activity.BaseActivity;
 
 public class Speed1 extends BaseActivity {
-    private TextView flow_rate_symbol;
-    private TextView area_symbol;
-    private TextView velocity_symbol;
-
-    private TextView flow_rate_unit;
-    private TextView area_unit;
-
-    private EditText flow_rate_value;
-    private EditText area_value;
-
-    private TextView formula;
-    private TextView result;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*this.vel.setOnClickListener(
-		 new OnClickListener() {
-		 public void onClick(View view) {
-
-		 if (Tools.isNull(vaz.getText().toString())) {
-		 vaz.setError(getText(R.string.null_field));
-		 verifyVaz = false;
-		 } else if (Tools.isDot(vaz.getText().toString())) {
-		 vaz.setError(getText(R.string.dot_value));
-		 verifyVaz = false;
-		 } else {
-		 vazao = Double.valueOf(vaz.getText().toString());
-		 verifyVaz = true;
-		 }
-
-
-		 if (Tools.isNull(ar.getText().toString())) {
-		 ar.setError(getText(R.string.null_field));
-		 verifyAr = false;
-		 } else if (Tools.isDot(ar.getText().toString())) {
-		 ar.setError(getText(R.string.dot_value));
-		 verifyAr = false;
-		 } else {
-		 area = Double.valueOf(ar.getText().toString());
-		 verifyAr = true;
-		 }
-
-
-		 if (verifyVaz && verifyAr) {
-		 res.setText(getText(R.string.speedp)
-		 + " "
-		 + "("
-		 + vazao
-		 + getText(R.string.vmc)
-		 + ") "
-		 + getText(R.string.division)
-		 + " ("
-		 + area
-		 + getText(R.string.amq)
-		 + ")");
-
-		 res.setText((CharSequence) res.getText().toString()
-		 + "\n"
-		 + getText(R.string.speedp)
-		 + " "
-		 + FF.speed1(vazao, area)
-		 + getText(R.string.speedmps));
-
-		 return;
-		 } else {
-		 return;
-		 }
-		 }
-		 });
-
-		 this.clear.setOnClickListener(
-		 new OnClickListener()
-		 {
-		 @Override
-		 public void onClick(View view)
-		 {
-		 vaz.setText(null);
-		 ar.setText(null);
-		 res.setText(null);
-
-		 return;
-		 }
-		 });*/
     }
 
     @Override
     protected void getViews() {
-        setToolBar((Toolbar) findViewById(R.id.toolbar));
-
-        flow_rate_symbol = findViewById(R.id.param_0_symbol);
-        area_symbol = findViewById(R.id.param_1_symbol);
-        velocity_symbol = findViewById(R.id.result_symbol);
-
-        flow_rate_value = findViewById(R.id.param_0_value);
-        area_value = findViewById(R.id.param_1_value);
-
-        flow_rate_unit = findViewById(R.id.param_0_unit);
-        area_unit = findViewById(R.id.param_1_unit);
-
-        formula = findViewById(R.id.formula);
-        result = findViewById(R.id.result);
     }
 
     @Override
@@ -120,26 +25,5 @@ public class Speed1 extends BaseActivity {
 
     @Override
     protected void initializeAttributes() {
-        flow_rate_symbol.setText("Q = ");
-        area_symbol.setText("A = ");
-        velocity_symbol.setText("v = ?");
-
-        flow_rate_unit.setText("m³/s");
-        area_unit.setText("m²");
-
-        formula.setText(R.string.fluid_flow_speed1_formula);
-
-        flow_rate_symbol.setVisibility(View.VISIBLE);
-        area_symbol.setVisibility(View.VISIBLE);
-        velocity_symbol.setVisibility(View.VISIBLE);
-
-        flow_rate_value.setVisibility(View.VISIBLE);
-        area_value.setVisibility(View.VISIBLE);
-
-        flow_rate_unit.setVisibility(View.VISIBLE);
-        area_unit.setVisibility(View.VISIBLE);
-
-        formula.setVisibility(View.VISIBLE);
-        result.setVisibility(View.VISIBLE);
     }
 }
