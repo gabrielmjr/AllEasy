1dea4c2c64221a368af2d4ba9dbe6ade6532c6c9
GabrielMJr <gmjplayys@gmail.com>
2023-05-18 01:34:04 +0200

    Standardized physic calculations template in urm >> time3

--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/mru/MRU_Time3.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/mru/MRU_Time3.java
@@ -3,9 +3,16 @@ package com.mjr.twaire.app.alleasy.activity.physic.kinematic.mru;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import com.mjr.twaire.app.alleasy.R;
-import com.mjr.twaire.app.alleasy.activity.BaseActivity;
+import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
+import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
+import com.mjr.twaire.app.alleasy.model.Data;
+import com.mjr.twaire.app.alleasy.model.Data;
+import java.util.ArrayList;
 
-public class MRU_Time3 extends BaseActivity {
+public class MRU_Time3 extends PhysicCalculationsBaseActivity {
+    private ArrayList<Data> datas;
+    private PhysicCalculationsTemplateAdapter adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,19 +20,27 @@ public class MRU_Time3 extends BaseActivity {
 
     @Override
     protected void initializeActivity() {
-        setContentView(R.layout.activity_physic_calculations);
+        setContentView(R.layout.template_physic_calculation);
     }
     
     @Override
     protected void getViews() {
+        super.getViews();
         setToolBar((Toolbar) findViewById(R.id.toolbar));
     }
 
     @Override
     protected void initializeAttributes() {
-        showNecessaryViews();
+        datas = new ArrayList<>();
+        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
+        super.initializeAttributes();
     }
     
-    private void showNecessaryViews() {
+    @Override
+    protected void setTemplateAttributes() {
+        datas.add(new Data("Si = ", "s"));
+        datas.add(new Data("S = ", "s"));
+        datas.add(new Data("∆v", "m/s"));
+        getDataContainer().setAdapter(adapter);
     }
 }
