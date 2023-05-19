655b28326fcfcad977f74c3e76eeb4f368ae7855
GabrielMJr <gmjplayys@gmail.com>
2023-05-19 20:22:40 +0200

    Standardized physic calculations template in vum >> acceleration 3

--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/muv/MUV_Acceleration3.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/muv/MUV_Acceleration3.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import com.mjr.twaire.app.alleasy.R;
 import com.mjr.twaire.app.alleasy.activity.BaseActivity;
+import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
+import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
+import com.mjr.twaire.app.alleasy.model.Data;
+import java.util.ArrayList;
 
-public class MUV_Acceleration3 extends BaseActivity {
+public class MUV_Acceleration3 extends PhysicCalculationsBaseActivity {
+    private ArrayList<Data> datas;
+    private PhysicCalculationsTemplateAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,15 +20,27 @@ public class MUV_Acceleration3 extends BaseActivity {
 
     @Override
     protected void initializeActivity() {
-        setContentView(R.layout.activity_physic_calculations);
-	}
+        setContentView(R.layout.template_physic_calculation);
+    }
 
-	@Override
+    @Override
     protected void getViews() {
-		setToolBar((Toolbar) findViewById(R.id.toolbar));
-	}
+        super.getViews();
+        setToolBar((Toolbar) findViewById(R.id.toolbar));
+    }
 
     @Override
     protected void initializeAttributes() {
+        datas = new ArrayList<>();
+        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
+        super.initializeAttributes();
+    }
+
+    @Override
+    protected void setTemplateAttributes() {
+        datas.add(new Data("vi = ", "m/s"));
+        datas.add(new Data("v = ", "m/s"));
+        datas.add(new Data("∆t = ", "s"));
+        getDataContainer().setAdapter(adapter);
     }
 }
