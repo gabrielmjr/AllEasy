41d44635be80d5263dc8222386db3f5599a502f0
GabrielMJr <gmjplayys@gmail.com>
2023-05-21 10:22:47 +0200

    Standardized physic calculations template in vum >> time 3

--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/muv/MUV_Time3.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/kinematic/muv/MUV_Time3.java
@@ -3,9 +3,14 @@ package com.mjr.twaire.app.alleasy.activity.physic.kinematic.muv;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import com.mjr.twaire.app.alleasy.R;
-import com.mjr.twaire.app.alleasy.activity.BaseActivity;
+import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
+import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
+import com.mjr.twaire.app.alleasy.model.Data;
+import java.util.ArrayList;
 
-public class MUV_Time3 extends BaseActivity {
+public class MUV_Time3 extends PhysicCalculationsBaseActivity {
+    private ArrayList<Data> datas;
+    private PhysicCalculationsTemplateAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +19,27 @@ public class MUV_Time3 extends BaseActivity {
 
     @Override
     protected void initializeActivity() {
-        setContentView(R.layout.activity_physic_calculations);
+        setContentView(R.layout.template_physic_calculation);
     }
 
 	@Override
     protected void getViews() {
+        super.getViews();
 		setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
 	}
 
     @Override
     protected void initializeAttributes() {
+        datas = new ArrayList<>();
+        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
+        super.initializeAttributes();
+    }
+
+    @Override
+    protected void setTemplateAttributes() {
+        datas.add(new Data("t = ", "s"));
+        datas.add(new Data("∆v = ", "m/s"));
+        datas.add(new Data("a = ", "m/s²"));
+        getDataContainer().setAdapter(adapter);
     }
 }
