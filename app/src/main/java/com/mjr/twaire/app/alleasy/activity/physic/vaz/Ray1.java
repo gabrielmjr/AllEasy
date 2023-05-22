94335eb1806e8d3738d05d83d830aedfa1d1e417
GabrielMJr <gmjplayys@gmail.com>
2023-05-22 11:00:12 +0200

    Standardized physic calculations template in fluid flow >> ray

--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/Ray1.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/Ray1.java
@@ -6,9 +6,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.mjr.twaire.app.alleasy.R;
-import com.mjr.twaire.app.alleasy.activity.BaseActivity;
+import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
+import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
+import com.mjr.twaire.app.alleasy.model.Data;
+import java.util.ArrayList;
 
-public class Ray1 extends BaseActivity {
+public class Ray1 extends PhysicCalculationsBaseActivity {
+    private ArrayList<Data> datas;
+    private PhysicCalculationsTemplateAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +22,27 @@ public class Ray1 extends BaseActivity {
 
     @Override
     protected void initializeActivity() {
-        setContentView(R.layout.activity_physic_calculations);
+        setContentView(R.layout.template_physic_calculation);
     }
 
     @Override
     protected void getViews() {
+        super.getViews();
         setToolBar((Toolbar) findViewById(R.id.toolbar));
     }
 
     @Override
     protected void initializeAttributes() {
+        datas = new ArrayList<>();
+        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
+        super.initializeAttributes();
+    }
+
+    @Override
+    protected void setTemplateAttributes() {
+        datas.add(new Data("Q = ", "m/s³"));
+        datas.add(new Data("v = ", "m/s"));
+        datas.add(new Data("π = ", "3.14"));
+        getDataContainer().setAdapter(adapter);
     }
 }
