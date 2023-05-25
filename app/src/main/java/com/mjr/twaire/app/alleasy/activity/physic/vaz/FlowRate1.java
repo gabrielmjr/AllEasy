36dd0efef5eeff076ed1b12641dbe3571f0163da
GabrielMJr <gmjplayys@gmail.com>
2023-05-25 14:53:13 +0200

    Refactored physic calculations activities in fluid flow

--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/FlowRate1.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/FlowRate1.java
@@ -1,44 +1,12 @@
 package com.mjr.twaire.app.alleasy.activity.physic.vaz;
 
-import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
-import com.mjr.twaire.app.alleasy.R;
 import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
-import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
 import com.mjr.twaire.app.alleasy.model.Data;
-import java.util.ArrayList;
 
 public class FlowRate1 extends PhysicCalculationsBaseActivity {
-    private ArrayList<Data> datas;
-    private PhysicCalculationsTemplateAdapter adapter;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void initializeActivity() {
-        setContentView(R.layout.template_physic_calculation);
-    }
-
-    @Override
-    protected void getViews() {
-        super.getViews();
-        setToolBar((Toolbar) findViewById(R.id.toolbar));
-    }
-
-    @Override
-    protected void initializeAttributes() {
-        datas = new ArrayList<>();
-        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
-        super.initializeAttributes();
-    }
-
     @Override
     protected void setTemplateAttributes() {
-        datas.add(new Data("V = ", "m³"));
-        datas.add(new Data("∆t = ", "s"));
-        getDataContainer().setAdapter(adapter);
+        getDatas().add(new Data("V = ", "m³"));
+        getDatas().add(new Data("∆t = ", "s"));
     }
 }
--- app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/Ray1.java
+++ app/src/main/java/com/mjr/twaire/app/alleasy/activity/physic/vaz/Ray1.java
@@ -1,48 +1,13 @@
 package com.mjr.twaire.app.alleasy.activity.physic.vaz;
 
-import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import com.mjr.twaire.app.alleasy.R;
 import com.mjr.twaire.app.alleasy.activity.physic.PhysicCalculationsBaseActivity;
-import com.mjr.twaire.app.alleasy.adapter.PhysicCalculationsTemplateAdapter;
 import com.mjr.twaire.app.alleasy.model.Data;
-import java.util.ArrayList;
 
 public class Ray1 extends PhysicCalculationsBaseActivity {
-    private ArrayList<Data> datas;
-    private PhysicCalculationsTemplateAdapter adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void initializeActivity() {
-        setContentView(R.layout.template_physic_calculation);
-    }
-
-    @Override
-    protected void getViews() {
-        super.getViews();
-        setToolBar((Toolbar) findViewById(R.id.toolbar));
-    }
-
-    @Override
-    protected void initializeAttributes() {
-        datas = new ArrayList<>();
-        adapter = new PhysicCalculationsTemplateAdapter(getApplicationContext(), datas);
-        super.initializeAttributes();
-    }
-
     @Override
     protected void setTemplateAttributes() {
-        datas.add(new Data("Q = ", "m/s³"));
-        datas.add(new Data("v = ", "m/s"));
-        datas.add(new Data("π = ", "3.14"));
-        getDataContainer().setAdapter(adapter);
+        getDatas().add(new Data("Q = ", "m/s³"));
+        getDatas().add(new Data("v = ", "m/s"));
+        getDatas().add(new Data("π = ", "3.14"));
     }
 }
